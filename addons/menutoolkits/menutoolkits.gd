be571ebb99e3b4eaf28c28f252fac80fa09107d4
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-08-29 01:37:50 +0200

    Creating custom type InputMappingButton

---
+++ addons/menutoolkits/menutoolkits.gd
@@ -0,0 +1,14 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	add_custom_type("InputMappingButton", "Button", preload("controls/scripts/input_mapping_button.gd"), preload("controls/icons/input_button_icon.png"))
+	pass
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	remove_custom_type("InputMappingButton")
+	pass
