9d9c2a6861d50acceec6bccd07c10feee8977597
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-08-29 01:39:02 +0200

    Change icon file's name

--- addons/menutoolkits/menutoolkits.gd
+++ addons/menutoolkits/menutoolkits.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	add_custom_type("InputMappingButton", "Button", preload("controls/scripts/input_mapping_button.gd"), preload("controls/icons/input_button_icon.png"))
+	add_custom_type("InputMappingButton", "Button", preload("controls/scripts/input_mapping_button.gd"), preload("controls/icons/input_mapping_button_icon.png"))
 	pass
 
 
