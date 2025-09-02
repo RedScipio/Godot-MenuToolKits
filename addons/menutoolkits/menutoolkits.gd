adf8d5fe18565fcc7675fa367d4dc8ea91ca1696
RedScipio <145475036+RedScipio@users.noreply.github.com>
2025-09-02 19:05:43 +0200

    Fix script

--- addons/menutoolkits/menutoolkits.gd
+++ addons/menutoolkits/menutoolkits.gd
@@ -5,7 +5,7 @@ extends EditorPlugin
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_custom_type("RemappingButton", "Button", preload("controls/scripts/remapping_button.gd"), preload("controls/icons/remapping_button_icon.png"))
-	add_custom_type("RemappingCatergoryGridView", "GridContainer", preload("controls/scripts/remapping_category.gd"), preload("controls/icons/remapping_button_icon.png"))
+	add_custom_type("RemappingCatergoryGridView", "GridContainer", preload("res://addons/menutoolkits/controls/scripts/remapping_category_grid_view.gd"), preload("controls/icons/remapping_button_icon.png"))
 	pass
 
 
